c7d276a312c762df6a279ac42bfa6cd4fd7abfe9
AgustinDiLeone <agustindileone@gmail.com>
2023-11-15 12:17:58 -0300

    Agrego paquete de sql

---
+++ SQL/AccesoDatos.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data.SqlClient;
+namespace SQL
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private static string cadena_conexion;
+        private SqlCommand comando; //con este objeto realizas la consulta
+        private SqlDataReader lector; //Contiene lo que te devuelve la consulta sql
+
+        static AccesoDatos()
+        {
+            AccesoDatos.cadena_conexion = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=test_bd;Integrated Security=True;Trust Server Certificate=True";
+            //AccesoDatos.cadena_conexion = Properties.Resources.miConexion;
+        }
+        public AccesoDatos()
+        {
+
+            this.conexion = new SqlConnection(AccesoDatos.cadena_conexion);
+        }
+    }
+}
