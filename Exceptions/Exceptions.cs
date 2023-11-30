ed87f416cdffcb28c770c348748e3396dd77d6e7
AgustinDiLeone <agustindileone@gmail.com>
2023-11-30 10:58:47 -0300

    agregando exceptions

--- Exceptions/Exceptions.cs
+++ Exceptions/Exceptions.cs
@@ -6,4 +6,20 @@
         {
         }
     }
+    public class NombreNoValido : Exception
+    {
+        public NombreNoValido() : base("Se ingreso un nombre no valido") { }
+    }
+    public class CuitNoValido : Exception
+    {
+        public CuitNoValido() : base("Se ingreso un cuit no valido") { }
+    }
+    public class UbicacionNoValido : Exception
+    {
+        public UbicacionNoValido() : base("Se ingreso una ubicacion no valida") { }
+    }
+    public class TipoNoValido : Exception
+    {
+        public TipoNoValido() : base("Se ingreso un tipo no valido") { }
+    }
 }
--- WinForms/FrmManejoCliente.cs
+++ WinForms/FrmManejoCliente.cs
@@ -1,10 +1,12 @@
 ﻿using Entidades;
+using Exceptions;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,7 @@ namespace WinForms
     public partial class FrmManejoCliente : Form
     {
         public Entidades.Cliente cliente;
+        public bool modificarCliente;
         public bool seCreoCliente;
         public string nombre;
         public long cuit;
@@ -32,29 +35,33 @@ namespace WinForms
             TxtNombre.Text = this.cliente.Nombre;
             TxtCuit.Text = this.cliente.Cuit.ToString();
             TxtUbicacion.Text = this.cliente.Ubicacion;
-            var tipos = ETipos.GetNames(typeof(ETipos));
-            CmbTipo.DropDownStyle = ComboBoxStyle.DropDownList;
-            CmbTipo.DataSource = tipos;
-            CmbTipo.SelectedItem = this.cliente.TipoCliente.ToString();
+            this.modificarCliente = true;
         }
 
         private void FrmAgregarCliente_Load(object sender, EventArgs e)
         {
+            var tipos = ETipos.GetNames(typeof(ETipos));
+            CmbTipo.DropDownStyle = ComboBoxStyle.DropDownList;
+            CmbTipo.DataSource = tipos;
+            if (modificarCliente)
+            {
+                CmbTipo.SelectedItem = this.cliente.TipoCliente.ToString();
+            }
 
         }
 
         private void BtnAceptar_Click(object sender, EventArgs e)
         {
-            bool verificacion = VerificarDatosGenerales();
-            if (verificacion)
+            try
             {
+                VerificarDatosGenerales();
                 this.seCreoCliente = true;
                 this.cliente = new Cliente(this.cuit, this.nombre, this.tipo, this.ubicacion);
                 this.DialogResult = DialogResult.OK;
             }
-            else
+            catch(Exception mensaje)
             {
-                return;
+                MessageBox.Show(mensaje.Message,"Error!!");
             }
         }
 
@@ -63,31 +70,31 @@ namespace WinForms
             this.DialogResult = DialogResult.Cancel;
         }
 
-        private bool VerificarDatosGenerales()
+        private void VerificarDatosGenerales()
         {
+            this.nombre = this.TxtNombre.Text;
+            if (this.nombre.Length == 0) 
+            {
+                throw new NombreNoValido();
+            }
             if (!(long.TryParse(this.TxtCuit.Text, out this.cuit)))
             {
-                MessageBox.Show("Ingrese un cuit valido", "ERROR");
-                return false;
+                throw new CuitNoValido();
             }
+            if (this.cuit <= 1000000)
+            {
+                throw new CuitNoValido();
+            }
+
+            this.ubicacion = this.TxtUbicacion.Text;
+            if (this.ubicacion.Length == 0)
+            {
+                throw new UbicacionNoValido();
+            }
+
             if (!(Enum.TryParse(this.CmbTipo.SelectedItem.ToString(), out this.tipo)))
             {
-                MessageBox.Show("Ingrese un tipo valido", "ERROR");
-                return false;
-            }
-
-            this.nombre = this.TxtNombre.Text;
-            this.ubicacion = this.TxtUbicacion.Text;
-
-
-            if (this.nombre.Length == 0 || this.ubicacion.Length == 0 || this.cuit <= 0)
-            {
-                MessageBox.Show("Ingrese datos validos", "ERROR");
-                return false;
-            }
-            else
-            {
-                return true;
+                throw new TipoNoValido();
             }
         }
     }
